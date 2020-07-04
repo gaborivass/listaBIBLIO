0397b8cafe3af42f26ed39c3ce99e9d3485f0b88
gaborivass <66798772+gaborivass@users.noreply.github.com>
2020-07-04 14:11:17 -0400

    commit de la clase lista nodo

---
+++ nodo.java
@@ -0,0 +1,17 @@
+
+package biblioteca;
+
+   public class nodo {
+    nodo prox;
+    nodo prev;
+    String nombre;
+    String apellido;
+    
+
+    nodo(String a, String b){
+        this.nombre=a;
+        this.apellido=b;
+        
+    }
+    
+}
